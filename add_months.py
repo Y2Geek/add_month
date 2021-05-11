bd9843ac6676e46f807475e7b4677c282f6dd935
Y2Geek <paul.ambler86@gmail.com>
2021-05-11 20:04:17 +0100

    initial-commit

---
+++ add_months.py
@@ -0,0 +1,59 @@
+"""A function to add a month to a datetime object"""
+
+from datetime import datetime
+
+def days_in_month(month, year):
+    """
+    Returns the number of days
+    in given month
+    """
+    # Months that have 31 days
+    long_months = [1, 3, 5, 7, 8, 10, 12]
+
+    # Now move date forward
+    if month == 2:
+        if year % 4 == 0:
+            return 29
+        else:
+            return 28
+    elif month in long_months:
+        return 31
+    else:
+        return 30
+
+def get_new_month(date):
+    """
+    Adds month to date, if current is December
+    returns a tuple with new month and new year
+    otherwise returns tuple with new_month and None
+    """
+    new_month = date.month + 1
+
+    if new_month > 12:
+        new_month -= 12
+        new_year = date.year +1
+        return (new_month, new_year)
+    else:
+        new_month = date.month + 1
+        return (new_month, None)
+
+
+def add_month(date):
+    """Moves date forward by given amount of months"""
+    new_details = get_new_month(date)
+    print(new_details)
+
+    if new_details[1] != None:
+        days_in_new_month = days_in_month(new_details[0], new_details[1])
+    else:
+        days_in_new_month = days_in_month(new_details[0], date.year)
+
+    if date.day > days_in_new_month:
+        day = days_in_new_month
+    else:
+        day = date.day
+    
+    if new_details[1] != None:
+        return datetime(new_details[1], new_details[0], day)
+    else:
+        return datetime(date.year, new_details[0], day)
